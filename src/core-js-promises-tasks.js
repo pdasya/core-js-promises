d295db58155b2803db731c7bd24746d54aeb22e3
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-04-11 15:57:30 +0900

    feat: solve getAllResults task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -119,8 +119,12 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const handledPromises = promises.map((promise) =>
+    promise.then((value) => value).catch(() => null)
+  );
+
+  return Promise.all(handledPromises);
 }
 
 /**
