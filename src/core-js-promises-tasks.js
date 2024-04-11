73c711658b8a8df0100e6b9aea3ea11b383cc39c
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-04-11 15:48:09 +0900

    feat: solve getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -56,8 +56,17 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.allSettled(promises).then((results) => {
+    const firstFulfilled = results.find(
+      (result) => result.status === 'fulfilled'
+    );
+    if (firstFulfilled) {
+      return firstFulfilled.value;
+    }
+
+    return Promise.reject(new Error('All promises were rejected'));
+  });
 }
 
 /**
